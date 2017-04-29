85a6d5818dff24bdadcaf2056c11d1a01680c586
Jingjing Duan <jduan@fitbit.com>
2017-04-29 11:01:10 -0700

    remove existing meals

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -16,6 +16,9 @@ class MealTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // use the edit button item provided by the table view controller
+        navigationItem.leftBarButtonItem = editButtonItem
+
         loadSampleMeals()
     }
 
@@ -59,17 +62,16 @@ class MealTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            meals.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
@@ -78,13 +80,11 @@ class MealTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
 
     // MARK: - Navigation
 
